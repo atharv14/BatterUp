e189cadfdb891027f01d6037d74c08273b13d9f8
Sarah Zhao <110712068+sarahlz01@users.noreply.github.com>
2025-01-18 00:38:37 -0800

    Update tailwind.config.js
    Added custom colors

--- frontend/BatterUp/tailwind.config.js
+++ frontend/BatterUp/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#041e42', 
+        'custom-gold': '#eece70',
+        'custom-red': '#bf0d3e',
+      },
+    },
   },
   plugins: [],
 }
