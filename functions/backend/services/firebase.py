9e9dddb830d20ebd38a2c3caa174094e928e5cab
Quoc Kien Ngo <156245273+KNQuoc@users.noreply.github.com>
2025-01-31 12:36:33 -0500

    reverted backend

--- functions/backend/services/firebase.py
+++ functions/backend/services/firebase.py
@@ -1,4 +1,4 @@
-from firebase_admin import credentials, firestore, auth, initialize_app
+from firebase_admin import credentials, firestore, initialize_app
 from core.config import settings
 import os
 
@@ -8,25 +8,12 @@ def init_firebase():
         cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
         initialize_app(cred)
         db = firestore.client()
-        print("🔥 Firestore DB Initialized Successfully")
         return db
     except Exception as e:
-        print(f"❌ Error initializing Firebase: {e}")
+        print(f"Error initializing Firebase: {e}")
         raise
 
 # Initialize Firestore DB
 db = init_firebase()
-
-def verify_firebase_token(id_token: str):
-    """
-    Verifies Firebase ID Token and returns decoded user info.
-    
-    :param id_token: Firebase Authentication Token
-    :return: Decoded user claims if token is valid, else None
-    """
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token  # Returns user details (UID, email, etc.)
-    except Exception as e:
-        print(f"❌ Token verification failed: {e}")
-        return None
+if db:
+    print("DB Initialized successfully")
